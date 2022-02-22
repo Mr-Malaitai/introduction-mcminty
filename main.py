08cb28e7cf609267064f966d5e3269c7ddb1eaa9
mcminty <sione_hav@student.asdah.school.nz>
2022-02-23 12:20:14 +1300

    added functions

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
+def display_option():
+
+    display_message = ""
+    
+    if option_chosen == 1:
+        display_message = "You have selected Option 1"
+    elif option_chosen == 2:
+        display_message = "You have selected Option 2"
+ 
+    message.configure(text = display_message)
+
 root = tk.Tk()
 
 option_chosen = tk.IntVar()
@@ -8,13 +19,13 @@ option_chosen = tk.IntVar()
 message = ttk.Label(root, text="Hello, world!")
 message.grid(row = 0, column =0)
 
-submit_button = ttk.Button(root, text = "Submit")
-submit_button.grid(row = 1, column = 0)
-
 option_one = ttk.Radiobutton(root, text = "Option One", variable = option_chosen, value = 1)
-option_one.grid(row = 2, column = 0)
+option_one.grid(row = 1, column = 0)
 
 option_two = ttk.Radiobutton(root, text = "Option Two", variable = option_chosen, value = 2)
-option_two.grid(row = 3, column = 0)
+option_two.grid(row = 2, column = 0)
+
+submit_button = ttk.Button(root, text = "Submit", command = display_option)
+submit_button.grid(row = 3, column = 0)
 
 root.mainloop()
